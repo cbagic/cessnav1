edcffe10a009acdeb7a1f245cd956a08ae2d9989
cbagic <blankess@nwfsc.edu>
2021-03-25 08:14:28 -0500

    Initial commit 3/25/2021 for scavenger hunt

---
+++ app-files/data.js
@@ -0,0 +1,587 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-welcome-to-cessna",
+      "name": "Welcome to Cessna!",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 0.14889685385320206,
+        "pitch": -0.11786861739898669,
+        "fov": 1.3108730938557924
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.5576974810833395,
+          "pitch": 0.04907155114878847,
+          "rotation": 0,
+          "target": "1-explore-the-shoreline"
+        },
+        {
+          "yaw": -0.7292587740785752,
+          "pitch": 0.13524843994651548,
+          "rotation": 0,
+          "target": "3-get-ready-to-build-reefs"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -0.36397184737508503,
+          "pitch": -0.5726728778555881,
+          "title": "Welcome to Cessna Landing!<br>",
+          "text": "Cessna Landing is located on Hogtown Bayou in Santa Rosa Beach, in the Southeast section of our bay. It's one of our best examples of a restored living shoreline! Previous Grasses In Classes students planted a lot of the smooth cordgrass you will see on your tour today.<br>"
+        },
+        {
+          "yaw": -0.5163677233089352,
+          "pitch": 0.05231075917030026,
+          "title": "This is our entry into the bayou!<br>",
+          "text": "When we build reefs and plant smooth cordgrass at Cessna Landing, we can unload our shell and plant bags here.<br>"
+        },
+        {
+          "yaw": 2.0882552244513333,
+          "pitch": -0.09819823115829429,
+          "title": "This is the Cessna Landing dock and boat launch.<br>",
+          "text": "Cessna Landing is a public park, so humans can enjoy it for fishing and recreation just like our wildlife.<br>"
+        },
+        {
+          "yaw": -2.8909708463475,
+          "pitch": -0.022720151618319306,
+          "title": "You can visit Cessna Landing too!<br>",
+          "text": "Cessna Address<br>"
+        },
+        {
+          "yaw": -0.07050843673630958,
+          "pitch": -0.6339758165065295,
+          "title": "Soundscape Placeholder 1<br>",
+          "text": "Text"
+        }
+      ]
+    },
+    {
+      "id": "1-explore-the-shoreline",
+      "name": "Explore the Shoreline",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 0.726626872258862,
+        "pitch": 0.10037312653850705,
+        "fov": 1.3108730938557924
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.981878310724472,
+          "pitch": -0.04419992872803746,
+          "rotation": 0,
+          "target": "0-welcome-to-cessna"
+        },
+        {
+          "yaw": 0.48331672486218125,
+          "pitch": 0.11592263831457394,
+          "rotation": 0,
+          "target": "2-explore-netting"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -0.0262676340007566,
+          "pitch": 0.2628053004263933,
+          "title": "Dipnetting game placeholder<br>",
+          "text": "Text"
+        },
+        {
+          "yaw": 2.2353588860310225,
+          "pitch": 0.003841427824095689,
+          "title": "Do you remember our erosion comparisons?<br>",
+          "text": "This is the place we took those photos! All this Spartina alterniflora was planted by Grasses In Classes students.<br>"
+        },
+        {
+          "yaw": 0.8873521732694911,
+          "pitch": 0.15673330996388302,
+          "title": "Here is an old reef.<br>",
+          "text": "This reef has collected so much sediment and restored so much shoreline that it has become buried! Our next project at Cessna is topping off this reef so it can continue to slow waves and protect the shoreline.<br>"
+        },
+        {
+          "yaw": 1.6051087572307292,
+          "pitch": 0.136926343590277,
+          "title": "Planting Video Placeholder<br>",
+          "text": "Text"
+        },
+        {
+          "yaw": 2.3800849586448507,
+          "pitch": -0.25475553080929814,
+          "title": "AmeriCorps member!<br>",
+          "text": "AmeriCorps members do a lot of planting at Cessna<br>"
+        }
+      ]
+    },
+    {
+      "id": "2-explore-netting",
+      "name": "Explore Netting",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 0.3112842640727944,
+        "pitch": -0.013828975786079667,
+        "fov": 1.3108730938557924
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.4883278909771107,
+          "pitch": 0.04496304944224683,
+          "rotation": 0,
+          "target": "1-explore-the-shoreline"
+        },
+        {
+          "yaw": 1.5741317286175605,
+          "pitch": -0.06863550874589563,
+          "rotation": 0,
+          "target": "0-welcome-to-cessna"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 2.049648415083558,
+          "pitch": 0.17337398276150573,
+          "title": "Dipnetting Video Placeholder<br>",
+          "text": "Text"
+        },
+        {
+          "yaw": -1.0897092798875097,
+          "pitch": 0.31126732538728774,
+          "title": "<div>Seine Netting Video Placeholder<br></div>",
+          "text": "Text"
+        },
+        {
+          "yaw": -0.29101750917442004,
+          "pitch": 0.0055052115183009676,
+          "title": "Cast Netting Video Placeholder<br>",
+          "text": "Text"
+        }
+      ]
+    },
+    {
+      "id": "3-get-ready-to-build-reefs",
+      "name": "Get Ready to Build Reefs",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.598927609450186,
+          "pitch": 0.06635161168022208,
+          "rotation": 0,
+          "target": "0-welcome-to-cessna"
+        },
+        {
+          "yaw": -0.27931584233750506,
+          "pitch": 0.08852146899324964,
+          "rotation": 0,
+          "target": "4-cross-the-water"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -2.405437379167102,
+          "pitch": -0.19555460066044006,
+          "title": "Passenger Van transporting shell bags.<br>",
+          "text": "After collecting recycled shell from local restaurants, we let it cure in the sun for six months, then put it in bags and drive it here from our working campus. Usually, we use a truck for this!<br>"
+        },
+        {
+          "yaw": 2.024921049531926,
+          "pitch": -0.07394515365838039,
+          "title": "Fishing dock<br>",
+          "text": "Cessna visitors often ask us questions about our reefs!<br>"
+        },
+        {
+          "yaw": 0.4226481248241356,
+          "pitch": -0.02517276501691157,
+          "title": "Jon Boat<br>",
+          "text": "We use jon boats to float our shell bags out to the reefs. It's a lot easier than carrying them!<br>"
+        },
+        {
+          "yaw": 0.002507242649029351,
+          "pitch": 0.1477471629084839,
+          "title": "AmeriCorps and CBA Members<br>",
+          "text": "These AmeriCorps and CBA members are hard at work protecting our bay!<br>"
+        },
+        {
+          "yaw": -1.1185155646717444,
+          "pitch": 0.1045894353620671,
+          "title": "Buried Reefs<br>",
+          "text": "This is one of our next focus areas at Cessna! We're going to top off these reefs and plant more smooth cordgrass.<br>"
+        },
+        {
+          "yaw": 0.004429120234549444,
+          "pitch": -0.1310101814999154,
+          "title": "The Cessna Point<br>",
+          "text": "Today, we're going to build a reef out at the point. We have to walk across the bayou with the jon boat to get there.<br>"
+        },
+        {
+          "yaw": 1.0894155672176886,
+          "pitch": -0.14097804343369447,
+          "title": "Houses &amp; Vacation Rentals<br>",
+          "text": "Locals, tourists, and wildlife all share the bay. Development on the shore has a huge impact on water quality and erosion.<br>"
+        },
+        {
+          "yaw": 0.4275680792852281,
+          "pitch": -0.4986903073645159,
+          "title": "Water Soundscape Placeholder<br>",
+          "text": "Text"
+        }
+      ]
+    },
+    {
+      "id": "4-cross-the-water",
+      "name": "Cross the Water",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": -1.0110323158592536,
+        "pitch": -0.009782685622507614,
+        "fov": 1.3108730938557924
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.4712154028425743,
+          "pitch": 0.04733886989120606,
+          "rotation": 0,
+          "target": "3-get-ready-to-build-reefs"
+        },
+        {
+          "yaw": -0.8880152610198682,
+          "pitch": 0.07506460785783631,
+          "rotation": 0,
+          "target": "5-keep-going-towards-the-reefs"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 1.8509428018262835,
+          "pitch": -0.004094205934439543,
+          "title": "We have to use waders to cross the bayou.<br>",
+          "text": "The silty organic soil makes it difficult to walk. Let's keep going towards the reefs!<br>"
+        },
+        {
+          "yaw": 0.06652389748309417,
+          "pitch": -0.18540256687646917,
+          "title": "Abandoned Boat<br>",
+          "text": "Sometimes people abandon old boats at Cessna. This makes it harder to work and recreate here.<br>"
+        },
+        {
+          "yaw": -2.0726892363750196,
+          "pitch": -0.3269360843129121,
+          "title": "Longleaf pines<br>",
+          "text": "The pine flatland ecosystem surrounds the bay. Land managers use controlled fires to keep this ecosystem safe and healthy.<br>"
+        },
+        {
+          "yaw": 3.1298455694473457,
+          "pitch": -0.08421495858043926,
+          "title": "Mysterious Boat!<br>",
+          "text": "We aren't really sure why this is here or whose it is, but we'll need to move it to build reefs here.<br>"
+        },
+        {
+          "yaw": -1.1464958026329093,
+          "pitch": 0.08024033257718699,
+          "title": "AmeriCorps and CBA<br>",
+          "text": "Look, they're already far ahead! Let's catch up!<br>"
+        }
+      ]
+    },
+    {
+      "id": "5-keep-going-towards-the-reefs",
+      "name": "Keep Going Towards the Reefs",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 0.6073026717523184,
+        "pitch": -0.08782648572118745,
+        "fov": 1.3108730938557924
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.5123534647209933,
+          "pitch": 0.07312270648562524,
+          "rotation": 0,
+          "target": "6-time-to-build-a-reef"
+        },
+        {
+          "yaw": -2.768702964368055,
+          "pitch": -0.06416594906192152,
+          "rotation": 0,
+          "target": "4-cross-the-water"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -0.8631325919175215,
+          "pitch": 0.10758438582996988,
+          "title": "Creek",
+          "text": "This creek is a freshwater input.<br>"
+        }
+      ]
+    },
+    {
+      "id": "6-time-to-build-a-reef",
+      "name": "Time To Build A Reef",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 0.7889600598300852,
+        "pitch": 0.020074625307703542,
+        "fov": 1.3108730938557924
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.050772509777852,
+          "pitch": 0.025978155112731827,
+          "rotation": 0,
+          "target": "7-around-the-point"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -0.00007229034597422412,
+          "pitch": -0.1200063533862128,
+          "title": "Brennan Wehrhahn<br>",
+          "text": "Brennan is an Ecology Technician at the Choctawhatchee Basin Alliance.<br>"
+        },
+        {
+          "yaw": 0.4445888216065814,
+          "pitch": -0.14858505275791245,
+          "title": "Chad Perko<br>",
+          "text": "Chad Perko is an Ecology Technician at the Choctawhatchee Basin Alliance.<br>"
+        },
+        {
+          "yaw": 1.4276958607585746,
+          "pitch": 0.11468549808349948,
+          "title": "We leave gaps in our reefs<br>",
+          "text": "This obeys government regulations and makes sure water can pass through and creatures won't get trapped.<br>"
+        },
+        {
+          "yaw": 0.8256501334722639,
+          "pitch": -0.12723454733913186,
+          "title": "These AmeriCorps members just used all their shell!<br>",
+          "text": "Now, it's time to go back for another load.<br>"
+        },
+        {
+          "yaw": 1.1638712587284967,
+          "pitch": 0.0915154302041099,
+          "title": "Reef Building video placeholder<br>",
+          "text": "Text"
+        },
+        {
+          "yaw": 1.6917274310083075,
+          "pitch": -0.01342418617574026,
+          "title": "We plant smooth cordgrass behind our reefs<br>",
+          "text": "Explanation why<br>"
+        }
+      ]
+    },
+    {
+      "id": "7-around-the-point",
+      "name": "Around the Point",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": -0.9199793999803738,
+        "pitch": 0.050249415059433034,
+        "fov": 1.3108730938557924
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.8109590427420166,
+          "pitch": 0.03707786932586288,
+          "rotation": 0,
+          "target": "6-time-to-build-a-reef"
+        },
+        {
+          "yaw": -1.0765899038902056,
+          "pitch": -0.21703901242003454,
+          "rotation": 0,
+          "target": "0-welcome-to-cessna"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -0.6750018297334588,
+          "pitch": -0.004119604974430757,
+          "title": "Bag Shaking Placeholder<br>",
+          "text": "Text"
+        },
+        {
+          "yaw": -0.13130411801986597,
+          "pitch": -0.062081150127959006,
+          "title": "Our reefs are 30 ft long<br>",
+          "text": "This follows government regulations<br>"
+        },
+        {
+          "yaw": -1.454927284200144,
+          "pitch": -0.0807776406493268,
+          "title": "We build reefs around the point.<br>",
+          "text": "The point is vulnerable to erosion, so we have to protect it.<br>"
+        },
+        {
+          "yaw": 2.9868145943673152,
+          "pitch": -0.10495496112915959,
+          "title": "This is the way out of Hogtown Bayou.<br>",
+          "text": "Cessna Landing connects to the Choctawhatchee Bay. You can find many more of our living shoreline restoration sites around the bay. Add website link.<br>"
+        }
+      ]
+    }
+  ],
+  "name": "Cessna Tour",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
